3df85669f46edc456b86614fb3fbcc238f55a2e1
ayush <ayushmatmotgill@gmail.com>
2023-02-22 23:04:24 -0800

    o

--- redux/actions/page.js
+++ redux/actions/page.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { LOGIN_FAIL, LOGIN_REQUEST,LOGIN_SUCCESS,REGISTER_USER_REQUEST,
   REGISTER_USER_SUCCESS,
-  REGISTER_USER_FAIL, } from '../constants/page';
+  REGISTER_USER_FAIL, SOCIAL_LOGIN_REQUEST, SOCIAL_LOGIN_SUCCESS, SOCIAL_LOGIN_FAIL } from '../constants/page';
 
  const login = (email, password) => async (dispatch) => {
 
@@ -25,8 +25,6 @@ import { LOGIN_FAIL, LOGIN_REQUEST,LOGIN_SUCCESS,REGISTER_USER_REQUEST,
     try {
       dispatch({ type: REGISTER_USER_REQUEST });
       const config = { headers: { "Content-Type": "application/json" } };
-
-      // const config = { headers: { "Content-Type": "multipart/form-data" } };
   
       const { data } = await axios.post('https://care-express-api.dthree.in/api/auth/register', userData, config);
 
@@ -38,8 +36,29 @@ import { LOGIN_FAIL, LOGIN_REQUEST,LOGIN_SUCCESS,REGISTER_USER_REQUEST,
       });
     }
   };
+
+
+export const socialLogin = () => async (dispatch) => {
+  try {
+    dispatch({ type: SOCIAL_LOGIN_REQUEST });
+
+    const { data } = await axios.get(`https://care-express-api.dthree.in/api/auth/socialLogin`);
+
+    dispatch({ type: SOCIAL_LOGIN_SUCCESS, payload: data.user });
+  } catch (error) {
+    dispatch({ type: SOCIAL_LOGIN_FAIL, payload: error.response.data.message });
+  }
+};
+
+
+
+
+
+
+
+
+
   
   export default login;
 
-  // "email": "sickrenders9@gmail.com",
-  // "password": "careuser"
+  
