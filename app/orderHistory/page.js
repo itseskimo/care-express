d7239099b3b4329f4920eaf92bf73755a63dbd65
ayush <ayushmatmotgill@gmail.com>
2023-02-27 03:58:55 -0800

    i

--- app/orderHistory/page.js
+++ app/orderHistory/page.js
@@ -5,76 +5,76 @@ import Head from '../head'
 const page = () => (
   <div className='bg-specialbg  '>
     <Head title='Order History' />
-    <main className='pt-10 ml-auto mr-auto w-[93%]'>
+    <main className='pt-8 ml-auto mr-auto w-[92%]'>
       <DashboardNav navTitle='Order History' />
       <section className='mt-16  overflow-hidden'>
         <table className='w-full '>
         <tbody>
           <tr className='text-left  '>
-            <th className='py-5 pl-10 font-semibold'>ORDER DATE</th>
-            <th className='py-5 font-semibold'>CARE TYPE</th>
-            <th className='py-5 font-semibold'>NO OF HOURS</th>
-            <th className='py-5 font-semibold'>RATE PER HOUR</th>
-            <th className='py-5 font-semibold'>TOTAL PRICE</th>
-            <th className='py-5 pr-10 font-semibold'>ACTIONS</th>
+            <th className='py-5 pl-10 font-semibold text-[14px] tracking-[0.15em]'>ORDER DATE</th>
+            <th className='py-5 font-semibold text-[14px] tracking-[0.15em]'>CARE TYPE</th>
+            <th className='py-5 font-semibold text-[14px] tracking-[0.15em]'>NO OF HOURS</th>
+            <th className='py-5 font-semibold text-[14px] tracking-[0.15em]'>RATE PER HOUR</th>
+            <th className='py-5 font-semibold text-[14px] tracking-[0.15em]'>TOTAL PRICE</th>
+            <th className='py-5 pr-10 font-semibold text-[14px] tracking-[0.15em]'>ACTIONS</th>
           </tr>
 
 
           <tr className='border-b-[1px] border-solid border-gray-300 bg-white'>
-            <td className='py-5 pl-10'>24 FEB 2023</td>
-            <td className='py-5'>Pet Express</td>
-            <td className='py-5'>10 Hours</td>
-            <td className='py-5'>55 zl/h</td>
-            <td className='py-5'>550 zl</td>
-            <td className='py-5 underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
+            <td className='py-5 pl-10 text-[14px]'>24 FEB 2023</td>
+            <td className='py-5 text-[14px]'>Pet Express</td>
+            <td className='py-5 text-[14px]'>10 Hours</td>
+            <td className='py-5 text-[14px]'>55 zl/h</td>
+            <td className='py-5 text-[14px]'>550 zl</td>
+            <td className='py-5 text-[14px] underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
           </tr>
           <tr className='border-b-[1px] border-solid border-gray-300 bg-white'>
-            <td className='py-5 pl-10'>24 FEB 2023</td>
-            <td className='py-5'>Pet Express</td>
-            <td className='py-5'>10 Hours</td>
-            <td className='py-5'>55 zl/h</td>
-            <td className='py-5'>550 zl</td>
-            <td className='py-5 underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
+            <td className='py-5 text-[14px] pl-10'>24 FEB 2023</td>
+            <td className='py-5 text-[14px]'>Pet Express</td>
+            <td className='py-5 text-[14px]'>10 Hours</td>
+            <td className='py-5 text-[14px]'>55 zl/h</td>
+            <td className='py-5 text-[14px]'>550 zl</td>
+            <td className='py-5 text-[14px] underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
           </tr>
           <tr className='border-b-[1px] border-solid border-gray-300 bg-white'>
-            <td className='py-5 pl-10'>24 FEB 2023</td>
-            <td className='py-5'>Pet Express</td>
-            <td className='py-5'>10 Hours</td>
-            <td className='py-5'>55 zl/h</td>
-            <td className='py-5'>550 zl</td>
-            <td className='py-5 underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
+            <td className='py-5 text-[14px] pl-10'>24 FEB 2023</td>
+            <td className='py-5 text-[14px]'>Pet Express</td>
+            <td className='py-5 text-[14px]'>10 Hours</td>
+            <td className='py-5 text-[14px]'>55 zl/h</td>
+            <td className='py-5 text-[14px]'>550 zl</td>
+            <td className='py-5 text-[14px] underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
           </tr>
           <tr className='border-b-[1px] border-solid border-gray-300 bg-white'>
-            <td className='py-5 pl-10'>24 FEB 2023</td>
-            <td className='py-5'>Pet Express</td>
-            <td className='py-5'>10 Hours</td>
-            <td className='py-5'>55 zl/h</td>
-            <td className='py-5'>550 zl</td>
-            <td className='py-5 underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
+            <td className='py-5 text-[14px] pl-10'>24 FEB 2023</td>
+            <td className='py-5 text-[14px]'>Pet Express</td>
+            <td className='py-5 text-[14px]'>10 Hours</td>
+            <td className='py-5 text-[14px]'>55 zl/h</td>
+            <td className='py-5 text-[14px]'>550 zl</td>
+            <td className='py-5 text-[14px] underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
           </tr>
           <tr className='border-b-[1px] border-solid border-gray-300 bg-white'>
-            <td className='py-5 pl-10'>24 FEB 2023</td>
-            <td className='py-5'>Pet Express</td>
-            <td className='py-5'>10 Hours</td>
-            <td className='py-5'>55 zl/h</td>
-            <td className='py-5'>550 zl</td>
-            <td className='py-5 underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
+            <td className='py-5 text-[14px] pl-10'>24 FEB 2023</td>
+            <td className='py-5 text-[14px]'>Pet Express</td>
+            <td className='py-5 text-[14px]'>10 Hours</td>
+            <td className='py-5 text-[14px]'>55 zl/h</td>
+            <td className='py-5 text-[14px]'>550 zl</td>
+            <td className='py-5 text-[14px] underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
           </tr>
           <tr className='border-b-[1px] border-solid border-gray-300 bg-white'>
-            <td className='py-5 pl-10'>24 FEB 2023</td>
-            <td className='py-5'>Pet Express</td>
-            <td className='py-5'>10 Hours</td>
-            <td className='py-5'>55 zl/h</td>
-            <td className='py-5'>550 zl</td>
-            <td className='py-5 underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
+            <td className='py-5 text-[14px] pl-10'>24 FEB 2023</td>
+            <td className='py-5 text-[14px]'>Pet Express</td>
+            <td className='py-5 text-[14px]'>10 Hours</td>
+            <td className='py-5 text-[14px]'>55 zl/h</td>
+            <td className='py-5 text-[14px]'>550 zl</td>
+            <td className='py-5 text-[14px] underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
           </tr>
           <tr className='bg-white'>
-            <td className='py-5 pl-10'>24 FEB 2023</td>
-            <td className='py-5'>Pet Express</td>
-            <td className='py-5'>10 Hours</td>
-            <td className='py-5'>55 zl/h</td>
-            <td className='py-5'>550 zl</td>
-            <td className='py-5 underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
+            <td className='py-5 text-[14px] pl-10'>24 FEB 2023</td>
+            <td className='py-5 text-[14px]'>Pet Express</td>
+            <td className='py-5 text-[14px]'>10 Hours</td>
+            <td className='py-5 text-[14px]'>55 zl/h</td>
+            <td className='py-5 text-[14px]'>550 zl</td>
+            <td className='py-5 text-[14px] underline underline-offset-2 decoration-[1px] text-blue cursor-pointer flex'><img className='pr-3' src='../images/business/Icons/business-express-separator-star.svg'/>Download Invoice</td>
           </tr>
          
           </tbody>
