cd282d463bd7458db405b64df4b16f0039f9df76
ayush <ayushmatmotgill@gmail.com>
2023-03-08 01:35:51 -0800

    i

--- app/reports/booking/page.js
+++ app/reports/booking/page.js
@@ -8,11 +8,23 @@ import {format} from 'date-fns'
 import {Calendar} from 'react-date-range';
 import {useState,useEffect} from 'react'
 import {useRouter} from 'next/navigation';
+import { useDispatch } from 'react-redux'
+import { getServicePricing } from '@/redux/actions/page'
 
 
 
 const page = () => {
+  const dispatch= useDispatch()
 
+  useEffect(()=>{
+   
+
+    if(localStorage.getItem('user')){
+      dispatch(getServicePricing())
+    }
+
+  },[])
+  
 
     const router = useRouter();
 
