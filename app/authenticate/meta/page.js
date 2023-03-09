1b8745fb2b35f131c95a02de972f708fb389b15b
ayush <ayushmatmotgill@gmail.com>
2023-03-08 16:41:54 -0800

    j

---
+++ app/authenticate/meta/page.js
@@ -0,0 +1,37 @@
+"use client"
+import queryString from 'query-string';
+import { socialLogin } from '@/redux/actions/page'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { useRouter } from 'next/navigation';
+
+
+const page = () => {
+
+  const dispatch= useDispatch()
+  const router = useRouter();
+
+  useEffect(()=>{
+    const urlParams = queryString.parse(window.location.search);
+
+    if (urlParams.error) {
+      console.log(`An error occurred: ${urlParams.error}`);
+    } else {
+      console.log(`The code is: ${urlParams.code}`);
+      dispatch(socialLogin(urlParams.code,"meta"))
+    }
+
+
+  },[])
+
+
+  return (
+    <div>
+
+
+
+    </div>
+  )
+}
+
+export default page
