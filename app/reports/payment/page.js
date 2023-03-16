5d70cadc94ab12c37887bbdfab3652ab28778453
ayush <ayushmatmotgill@gmail.com>
2023-03-16 02:24:33 -0700

    h

---
+++ app/reports/payment/page.js
@@ -0,0 +1,13 @@
+"use client"
+import { PaymentElement } from '@stripe/react-stripe-js'
+const page = () => {
+  return (
+    <div>
+
+<PaymentElement/>
+
+    </div>
+  )
+}
+
+export default page
