becc6d1c676ee09eca306edd60c99030430dc74c
ayush <ayushmatmotgill@gmail.com>
2023-03-26 08:33:22 -0700

    update: booking section uprade

--- app/booking/additionalRequirements/page.js
+++ app/booking/additionalRequirements/page.js
@@ -9,23 +9,53 @@ import Head from '../../head'
 
 const page = () => {
 
-const router = useRouter();
-const [saveCheckBoxesText,setSaveCheckBoxesText]=useState([])
+  const router = useRouter();
 
-function checkboxSaver(id){
-   let validator = saveCheckBoxesText.includes(id)
-
-   if(validator){
-    setSaveCheckBoxesText([...new Set ([...saveCheckBoxesText.filter((item)=> item !== id)])].sort((a,b)=>b.length-a.length))
-   }else{
-    setSaveCheckBoxesText([...new Set ([id,...saveCheckBoxesText])].sort((a,b)=>b.length-a.length))
-   }
-}
-
-function saveAdditionalRequirements(){
-  localStorage.setItem('additionalRequirements',additionalRequirements)
-  localStorage.setItem('additionalServicesArr', saveCheckBoxesText)
+  const [saveCheckBoxesText,setSaveCheckBoxesText]=useState([])
+console.log(saveCheckBoxesText)
+  function checkboxSaver(id){
+     let validator = saveCheckBoxesText.includes(id)
+  
+     if(validator){
+      setSaveCheckBoxesText([...new Set ([...saveCheckBoxesText.filter((item)=> item !== id)])].sort((a,b)=>b.length-a.length))
+     }else{
+      setSaveCheckBoxesText([...new Set ([id,...saveCheckBoxesText])].sort((a,b)=>b.length-a.length))
+     }
   }
+  
+  function saveAdditionalRequirements(){
+    localStorage.setItem('additionalRequirements',additionalRequirements)
+    localStorage.setItem('additionalServicesArr', saveCheckBoxesText)
+    }
+
+
+const [additionalRequirements,setAdditionalRequirements]=useState('')
+const [hours,setHours]=useState('')
+const [cost,setCost]=useState('')
+
+
+useEffect(()=>{
+
+if(localStorage.getItem('user')){
+
+  let hours= localStorage.getItem('hours')
+  setHours(hours)
+  let cost= localStorage.getItem('cost')
+  setCost(cost)
+
+  if(localStorage.getItem('additionalRequirements')){
+  const additionalRequirements=  localStorage.getItem('additionalRequirements')
+  setAdditionalRequirements(additionalRequirements)
+  }
+
+  if(localStorage.getItem('additionalServicesArr')){
+  const saveCheckBoxesText=  localStorage.getItem('additionalServicesArr')
+  setSaveCheckBoxesText([...saveCheckBoxesText.split(',')])
+  }
+
+}
+},[])
+
 
   const navDetails={
     clicked:'Booking',
@@ -58,47 +88,25 @@ function saveAdditionalRequirements(){
     FbIcon:'../images/business/Icons/facebook.svg',
   }
 
-const [additionalRequirements,setAdditionalRequirements]=useState('')
-const [hours,setHours]=useState('')
-const [cost,setCost]=useState('')
 
-
- useEffect(()=>{
-  
-  if(localStorage.getItem('user')){
-
-    let hours= localStorage.getItem('hours')
-    setHours(hours)
-    let cost= localStorage.getItem('cost')
-    setCost(cost)
-
-    if(localStorage.getItem('additionalRequirements')){
-    const additionalRequirements=  localStorage.getItem('additionalRequirements')
-    setAdditionalRequirements(additionalRequirements)
-
-    }
-  }
- },[])
-  
   return (
     <>
-            <Head title='Additional Requirements'/>
+    <div className='bg-specialbg  h-max'>
+    <Head title='Additional Requirements'/>
 
-    <Navbar color={'bg-blue'} {...navDetails}/>
-   
-{/* ----------------------------------------------------------------------------------------------------------------- */}
-<main className='bg-specialbg overflow-hidden'>
-<main className='mx-4 sm:mx-14 '>
-  {/* ------------RESPONSIBLE FOR MARGIN---------------- */}
+<Navbar color={'bg-blue'} {...navDetails}/>
+    <main className='py-8 ml-auto mr-auto w-[92%]'>
+    
+    <BookingHeader active={2}/>
 
 
-  <BookingHeader active={2}/>
-
-<section className='bg-white h-max rounded-[16px] mt-10'>
+    <section className='bg-white h-max rounded-[16px] mt-10'>
 
 <main className=' pt-6'>
 
-<section className=' flex items-center justify-between md:justify-start md:gap-28  pb-5 mx-2 sm:mx-3 smd:mx-14 xlg:mx-28 relative'>
+<section className=' flex items-center justify-between  pb-5 mx-2 sm:mx-3 smd:mx-14 xlg:mx-28 '>
+
+<section className='flex gap-28'>
 <div className=''>
   <h6 className='tracking-[0.12em] text-xs font-semibold'>SELECT PLAN</h6>
   <h6 className='text-[19px]  xlsm:text-[24px] font-bold'>{hours} hours</h6>
@@ -108,9 +116,10 @@ const [cost,setCost]=useState('')
   <h6 className='tracking-[0.12em] text-xs font-semibold'>TOTAL PRICE</h6>
   <h6 className='text-[19px]  xlsm:text-[24px] font-bold'>{`${parseInt(hours)*parseInt(cost)} zł`}</h6>
 </div>
+</section>
 
+  <button className=' shrink-0 border-[1px] border-solid  border-gray-300 flex px-3 xlsm:px-5 py-[10px] text-sm font-semibold rounded-[50px]' onClick={()=>router.push('/reports/booking')}><img src='../images/booking/Edit.svg' className='pr-2'/>Edit Plan</button>
 
-<button className='md:absolute right-0 shrink-0 border-[1px] border-solid  border-gray-300 flex px-3 xlsm:px-5 py-[10px] text-sm font-semibold rounded-[50px]' onClick={()=>router.push('/booking/plan')}><img src='../images/booking/Edit.svg' className='pr-2'/>Edit Plan</button>
 </section>
 
 </main>
@@ -118,21 +127,22 @@ const [cost,setCost]=useState('')
 <section className='border-[1px]  border-solid  border-gray-300' ></section>
 
 {/* ------------------------------------------------------------------------------------ */}
+
 <main className='mx-2 sm:mx-3 smd:mx-14 xlg:mx-28 py-10'>
 
 <h6 className='tracking-[0.02em] text-[16px] font-semibold mb-2'>Additional Services</h6>
 
 <section className='flex flex-col md:flex-row gap-4 md:gap-20 lg:gap-32 xlg:gap-48'>
-<div className='flex items-center ' onClick={()=>checkboxSaver('Language Tutoring')}>
-    <input type='checkbox' className='w-4 h-4 mr-2 select-none' ></input>
+<div className='flex items-center  cursor-pointer' onClick={()=>checkboxSaver('Language Tutoring')}>
+    <input type='checkbox' className='w-4 h-4 mr-2 select-none' checked={saveCheckBoxesText.includes('Language Tutoring')} onChange={(e) =>e.target.checked}></input>
     <h6 className='tracking-[0.02em] text-[16px] font-semibold whitespace-nowrap'>Language Tutoring</h6>
 </div>
-<div className='flex items-center'  onClick={()=>checkboxSaver('VAS #2')}>
-    <input type='checkbox' className='w-4 h-4 mr-2 select-none' ></input>
+<div className='flex items-center cursor-pointer'  onClick={()=>checkboxSaver('VAS #2')}>
+    <input type='checkbox' className='w-4 h-4 mr-2 select-none' checked={saveCheckBoxesText.includes('VAS #2')} onChange={(e) =>e.target.checked}></input>
     <h6 className='tracking-[0.02em] text-[16px] font-semibold whitespace-nowrap'>VAS #2</h6>
 </div>
-<div className='flex items-center'  onClick={()=>checkboxSaver('VAS #3')}>
-    <input type='checkbox' className='w-4 h-4 mr-2 select-none ' ></input>
+<div className='flex items-center cursor-pointer'  onClick={()=>checkboxSaver('VAS #3')}>
+    <input type='checkbox' className='w-4 h-4 mr-2 select-none' checked={saveCheckBoxesText.includes('VAS #3')} onChange={(e) =>e.target.checked}></input>
     <h6 className='tracking-[0.02em] text-[16px] font-semibold whitespace-nowrap'>VAS #3</h6>
 </div>
 
@@ -140,17 +150,17 @@ const [cost,setCost]=useState('')
 
 <h6 className='tracking-[0.02em] text-[16px] font-semibold mt-6 mb-3'>Any special needs or requirements</h6>
 
-<textarea style={{border:'1px solid #ABABAB'}} value={additionalRequirements} onChange={(e)=>setAdditionalRequirements(e.target.value)} placeholder='Tell us anything else we need to know, like allergies etc.' className='  outline-none bg-inputbg w-full px-4 pt-4  pb-[200px] overflow-hidden rounded-[12px]'></textarea>
+<textarea style={{border:'1px solid #ABABAB'}} value={additionalRequirements} onChange={(e)=>setAdditionalRequirements(e.target.value)} placeholder='Tell us anything else we need to know, like allergies etc.' className='resize-none  outline-none bg-inputbg w-full px-4 pt-4  pb-[200px] overflow-hidden rounded-[12px]'></textarea>
 
 <div className='flex justify-between'>
 <Link href={{pathname:'/booking/contact'}}><button className='bg-ligrey rounded-[50px] px-9 py-[8px] text-black text-[18px] font-semibold mt-10' >Back</button></Link>
-  <Link href={{pathname:'./booking/summary'}}><button className='bg-blue rounded-[50px] px-9 py-[8px] text-white text-[18px] font-semibold mt-10' onClick={saveAdditionalRequirements}>Next</button></Link>
+<Link href={{pathname:'/booking/summary'}}><button className='bg-blue rounded-[50px] px-9 py-[8px] text-white text-[18px] font-semibold mt-10' onClick={saveAdditionalRequirements}>Next</button></Link>
 </div>
 
 
 </main>
 
- 
+
 
 {/* ------------------------------------------------------------------------------------ */}
 
@@ -161,15 +171,8 @@ const [cost,setCost]=useState('')
 
 
 
-</main>
-</main>
-{/* ----------------------------------------------------------------------------------------------------------------- */}
-
-
-
-    
-{/* ----------------------------------------------------------------------------------------------------------------- */}
-
+    </main>
+  </div>
 
  <Footer {...footerDetails}/>
 
--- app/booking/plan/page.js
+++ app/booking/plan/page.js
@@ -2,101 +2,119 @@
 import Navbar from '../../Components/navbar/navbar'
 import Footer from '../../Components/footer/footer'
 import BookingHeader from '../../Components/bookingHeader/page'
+import Head from '../../head'
 import 'react-date-range/dist/styles.css'; 
 import 'react-date-range/dist/theme/default.css';
-import {format} from 'date-fns'
 import {Calendar} from 'react-date-range';
-import {useState,useEffect} from 'react'
+import {useState,useEffect, useRef} from 'react'
 import {useRouter} from 'next/navigation';
-import Head from '../../head'
 import { useDispatch, useSelector } from 'react-redux'
 import { getServicePricing } from '@/redux/actions/page'
 import ErrorModal from '../../Components/errorModal/page'
 
 
 const page = () => {
-
   const { orders}  = useSelector((state) => state.orders);
+
   const dispatch= useDispatch()
   const router = useRouter();
+  const closeCalendarRef = useRef(null);
+
+  const handleCalendarClose  = (e) => {
+    if (!closeCalendarRef.current.contains(e.target)) {
+      setToggleCalendar(false);
+    }
+  }
+  
+
+    const [careType,setCareType]=useState('')
+    const [selectPlanId,setSelectPlanId]=useState('')
+    const [hours,setHours]=useState('')
+    const [cost,setCost]=useState('')
+
+
+    const [togglecalendar,setToggleCalendar]=useState(false)
+
+    const [date, setDate] = useState(new Date(new Date().setDate(new Date().getDate() + 1)))
+    const [utilityDate, setUtilityDate] = useState('')
+
+    let dateDisplay = utilityDate ? utilityDate : JSON.stringify( date).slice(1,11).split("-").filter(item=>!!item).reverse().join("/")
+
+    const closeErrorRef = useRef(null);
+  
+    const handleErrorClose = (e) => {   
+    if(closeErrorRef.current === e.target) setErrorModal(false) 
+    }
 
   useEffect(()=>{
     if(localStorage.getItem('user')){
-      dispatch(getServicePricing())
+        dispatch(getServicePricing())
+        
+    if(localStorage.getItem('careType')){
+      const careType = localStorage.getItem('careType')
+      setCareType(careType)
+      }  
 
-      if(localStorage.getItem('plan')){
+    if(localStorage.getItem('plan')){
       const plan = localStorage.getItem('plan')
       const hours = localStorage.getItem('hours')
       const cost = localStorage.getItem('cost')
       extractText(plan, hours , cost)
       }
 
-      if(localStorage.getItem('careType')){
-      const careType = localStorage.getItem('careType')
-      setCareType(careType)
+    if(localStorage.getItem('dateDisplay')){
+      const dateDisplay = localStorage.getItem('dateDisplay')
+      setUtilityDate(dateDisplay)
       }
+      }
+      document.addEventListener("click", handleErrorClose, true);
+      document.addEventListener("click", handleCalendarClose, true);
+      return () => {document.removeEventListener("click", handleCalendarClose , true) 
+                    document.removeEventListener("click", handleErrorClose , true)
+      }
+    },[])
 
 
-
-    }
-  },[])
-  
-  const [careType,setCareType]=useState('nanny')
-  const [selectPlanId,setSelectPlanId]=useState('')
-  const [hours,setHours]=useState('')
-  const [cost,setCost]=useState('')
-
-  const [time,setTime]=useState('')
-
-
-  const [calendarDate,setCalendarDate]=useState('')
-  const [togglecalendar,setToggleCalendar]=useState(false)
-  const [date, setDate] = useState(null);
-
-
-
-  function formSubmit(e){
-    e.preventDefault();
-    if(calendarDate !== '' && selectPlanId !== '' && careType !== ''){
-    localStorage.setItem('hours', hours)
-    localStorage.setItem('cost', cost)
-    localStorage.setItem('careType', careType)
-    localStorage.setItem('calendarDate', time)
-    router.push('/booking/contact')
-    }else{
+    function formSubmit(e){
+      e.preventDefault();
+      if( selectPlanId !== '' && careType !== ''){
+      localStorage.setItem('hours', hours)
+      localStorage.setItem('cost', cost)
+      localStorage.setItem('careType', careType)
+      localStorage.setItem('dateDisplay', dateDisplay)
+      router.push('/booking/contact')
+      }else{
       setErrorModal(true)
+      }
     }
-  }
-  
-  function calendar(e){
-    const calendarCalc=['null',"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-   
-    const calendar= document.getElementById('extractCalendarTime')
-    let dateCalc= calendar.value.substring(0,1) === '0' ? calendar.value.substring(1,2) : calendar.value.substring(0,2)
-    let monthCalc = calendarCalc[calendar.value.substring(3,4) === '0' ? calendar.value.substring(4,5) : calendar.value.substring(3,5)] 
-    let yearCalc= `${calendar.value.substring(6,10)}`
 
-    const finalDate=`${dateCalc}` + '' + `${monthCalc}` + '' + `${yearCalc}`
-
-    setTime(finalDate)
-    setCalendarDate(calendar.value)
-
-    if(e.target.outerHTML !== '<i></i>' &&  e.target.innerHTML !== '<i></i>'){
-       setToggleCalendar(!togglecalendar)
+    
+    function calendar(){
+      if(dateDisplay === utilityDate)localStorage.removeItem('dateDisplay')
+      utilityDate.length > 0 &&  setUtilityDate('')  
+      setToggleCalendar(true)
     }
+
+    
+    function extractText(id, hours , rpHours){
+      localStorage.setItem('plan', id)
+      setSelectPlanId(id)
+      setHours(hours)
+      setCost(rpHours)
+    }
+
+    const [errorModal, setErrorModal]= useState(false)
+
+
+  function careTypeSetter(id){
+  setCareType(id)
+  let res = orders?.some( order => order['service'] === id )
+  if(!res){
+  localStorage.removeItem('plan')
+  localStorage.removeItem('careType')
+  setSelectPlanId('')
   }
-
-
-  
-  
-  
-  function extractText(id, hours , rpHours){
-    localStorage.setItem('plan', id)
-    setSelectPlanId(id)
-    setHours(hours)
-    setCost(rpHours)
   }
-
 
   const navDetails={
     clicked:'Booking',
@@ -128,39 +146,31 @@ const page = () => {
     FbIcon:'../images/business/Icons/facebook.svg',
   }
 
-  const [errorModal, setErrorModal]= useState(false)
 
-  setTimeout(() => {
-    errorModal && setErrorModal(false)
-  }, 2000);
-  
   return (
-    <>
-            <Head title='Select Plan'/>
+    <div className='bg-specialbg  h-max' >
+
+    {errorModal && <ErrorModal text='Please Select All Fields!' refState={closeErrorRef}/>}
 
     <Navbar color={'bg-blue'} {...navDetails}/>
- 
-    {errorModal && <ErrorModal text='Please Select All Fields!'/>}
 
-   
-  {/* ----------------------------------------------------------------------------------------------------------------- */}
-  <main className='bg-specialbg overflow-hidden'>
-  <main className='mx-4 sm:mx-14 '>
-  {/* ------------RESPONSIBLE FOR MARGIN---------------- */}
+    <Head title='Select Plan'/>
+    <main className='py-8 ml-auto mr-auto w-[92%]'>
+      
+
+    <BookingHeader active={0}/>
 
 
-  <BookingHeader active={0}/>
+<form className='bg-white h-max rounded-[16px] mt-10 ' onSubmit={formSubmit}>
 
-  <form className='bg-white h-max rounded-[16px] mt-10 ' onSubmit={formSubmit}>
-
-<main className='mx-28 py-10'>
+<main className='mx-32 py-10'>
 
 <div className=''>
 
 <h6 className='font-semibold tracking-[0.02em] text-[16px]'>Select Care Type</h6>
 <section className='mt-2 flex flex-wrap gap-9'  >
 
-  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'nanny' ? 'border-bookingblue' : 'border-bookingborder	'}`} id='nanny' onClick={(e)=>setCareType(e.target.id)}>
+  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'nanny'  ? 'border-bookingblue' : 'border-bookingborder	'}`} id='nanny' onClick={(e)=>careTypeSetter(e.target.id)}>
     <nav className={`bg-grey absolute w-full h-full select-none pointer-events-none rounded-[12px] ${careType === 'nanny' ? 'block' : 'hidden'}`} >
     <img src='../images/booking/check.svg' className=' absolute top-[50%]  left-[50%] -translate-y-[50%] -translate-x-[50%] select-none pointer-events-none'/>
     </nav>
@@ -169,7 +179,7 @@ const page = () => {
   </div>
 
 
-  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'senior' ? 'border-bookingblue' : 'border-bookingborder	'}`} id='senior' onClick={(e)=>setCareType(e.target.id)}>
+  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'senior' ? 'border-bookingblue' : 'border-bookingborder	'}`} id='senior' onClick={(e)=>careTypeSetter(e.target.id)}>
   <nav className={`bg-grey absolute w-full h-full select-none pointer-events-none rounded-[12px] ${careType === 'senior' ? 'block' : 'hidden'}`}>
     <img src='../images/booking/check.svg' className=' absolute top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%] select-none pointer-events-none'/>
   </nav>
@@ -178,7 +188,7 @@ const page = () => {
   </div>
 
 
-  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'pet' ? 'border-bookingblue' : 'border-bookingborder	'}`} id='pet' onClick={(e)=>setCareType(e.target.id)}>
+  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'pet' ? 'border-bookingblue' : 'border-bookingborder	'}`} id='pet' onClick={(e)=>careTypeSetter(e.target.id)}>
   <nav className={`bg-grey absolute w-full h-full select-none pointer-events-none rounded-[12px] ${careType === 'pet' ? 'block' : 'hidden'}`}>
     <img src='../images/booking/check.svg' className=' absolute top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%] select-none pointer-events-none' />
   </nav>
@@ -187,7 +197,7 @@ const page = () => {
   </div>
 
 
-  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'special' ? 'border-bookingblue' : 'border-bookingborder	'}`} id='special' onClick={(e)=>setCareType(e.target.id)}>
+  <div className={`border-[1px] border-solid w-max rounded-[12px] relative cursor-pointer ${careType === 'special' ? 'border-bookingblue' : 'border-bookingborder	'}`} id='special' onClick={(e)=>careTypeSetter(e.target.id)}>
   <nav className={`bg-grey absolute w-full h-full select-none pointer-events-none rounded-[12px] ${careType === 'special' ? 'block' : 'hidden'}`} >
     <img src='../images/booking/check.svg' className=' absolute top-[50%] left-[50%] -translate-y-[50%] -translate-x-[50%] select-none pointer-events-none' />
   </nav>
@@ -198,49 +208,47 @@ const page = () => {
 </div>
 
 {/* --------------------------------------------------------------------------------------- */}
-
 <div>
-{orders?.some( order => order['service'] === careType) && <h6 className='font-semibold tracking-[0.02em] text-[16px] my-5'>Choose Plan</h6>}
+  {orders?.some( order => order['service'] === careType) && <h6 className='font-semibold tracking-[0.02em] text-[16px] my-5'>Choose Plan</h6>}
 <section className='mt-2 flex flex-wrap gap-9' >
 
 {orders && orders.map((item)=>{
 
-if(item.most_popular === false){
+ if(item.most_popular === false){
 
- if(item.service === careType){
-   return <div key={item._id} className={`border-[1px] border-solid ${selectPlanId === item._id? 'border-blue' : 'border-bookingborder' } 	w-[196px] h-[180px] rounded-[12px] relative cursor-pointer`} onClick={()=>extractText(item._id, item.hours,item.rate_per_hour)}>
-   <section className='font-semibold flex flex-col pl-2 pt-9  rounded-[12px] select-none pointer-events-none' >
-   <h6 className='text-2xl font-medium mb-3 select-none pointer-events-none' >{item.hours} hours</h6>
-   <h6 className=' font-bold text-2xl select-none pointer-events-none' >{item.rate_per_hour} zł / h</h6>
-   </section>
-     <img src='../images/booking/check.svg' className={`absolute bottom-3 select-none pointer-events-none left-3 ${selectPlanId === item._id ? 'block' : 'hidden'}`} />
- </div>
- }
+  if(item.service === careType){
+    return <div key={item._id} className={`border-[1px] border-solid ${selectPlanId === item._id? 'border-blue' : 'border-bookingborder' } 	w-[196px] h-[180px] rounded-[12px] relative cursor-pointer`} onClick={()=>extractText(item._id, item.hours,item.rate_per_hour)}>
+    <section className='font-semibold flex flex-col pl-2 pt-9  rounded-[12px] select-none pointer-events-none' >
+    <h6 className='text-2xl font-medium mb-3 select-none pointer-events-none' >{item.hours} hours</h6>
+    <h6 className=' font-bold text-2xl select-none pointer-events-none' >{item.rate_per_hour} zł / h</h6>
+    </section>
+      <img src='../images/booking/check.svg' className={`absolute bottom-3 select-none pointer-events-none left-3 ${selectPlanId === item._id ? 'block' : 'hidden'}`} />
+  </div>
+  }
 
-}else{
+ }else{
 
- if(item.service === careType){
+  if(item.service === careType){
 
- return <div className={`border-[1px] border-solid 	w-[196px] h-[180px] ${selectPlanId === item._id? 'border-blue' : 'border-bookingborder' } rounded-[12px] relative cursor-pointer`} onClick={()=>extractText(item._id, item.hours,item.rate_per_hour)}>
- <nav className='flex bg-green w-max rounded-lg py-1 px-2 mt-2 ml-2 select-none pointer-events-none' >
- <img src='../images/booking/Star.svg' className='pr-2 select-none pointer-events-none' />
- <h6 className='text-xs font-bold text-white select-none pointer-events-none' >MOST POPULAR</h6>
- </nav>
- <section className='font-semibold flex flex-col pl-2 pt-[6px] select-none pointer-events-none rounded-[12px]' >
- <h6 className='text-2xl font-medium mb-3 select-none pointer-events-none' >{item.hours} hours</h6>
- <h6 className=' font-bold text-2xl select-none pointer-events-none' >{item.rate_per_hour} zł / h</h6>
- </section>
- <img src='../images/booking/check.svg' className={`absolute select-none pointer-events-none  bottom-3 left-3 ${selectPlanId === item._id? 'block' : 'hidden'}`} />
+  return <div className={`border-[1px] border-solid 	w-[196px] h-[180px] ${selectPlanId === item._id? 'border-blue' : 'border-bookingborder' } rounded-[12px] relative cursor-pointer`} onClick={()=>extractText(item._id, item.hours,item.rate_per_hour)}>
+  <nav className='flex bg-green w-max rounded-lg py-1 px-2 mt-2 ml-2 select-none pointer-events-none' >
+  <img src='../images/booking/Star.svg' className='pr-2 select-none pointer-events-none' />
+  <h6 className='text-xs font-bold text-white select-none pointer-events-none' >MOST POPULAR</h6>
+  </nav>
+  <section className='font-semibold flex flex-col pl-2 pt-[6px] select-none pointer-events-none rounded-[12px]' >
+  <h6 className='text-2xl font-medium mb-3 select-none pointer-events-none' >{item.hours} hours</h6>
+  <h6 className=' font-bold text-2xl select-none pointer-events-none' >{item.rate_per_hour} zł / h</h6>
+  </section>
+  <img src='../images/booking/check.svg' className={`absolute select-none pointer-events-none  bottom-3 left-3 ${selectPlanId === item._id? 'block' : 'hidden'}`} />
 </div>
 
+  }
+
+ 
  }
 
-
-}
-
 }) }
-  
-  
+ 
 </section>
 </div>
 
@@ -252,21 +260,21 @@ if(item.most_popular === false){
   
 <div className='min-w-[300px] sm:min-w-[330px] lg:max-w-none w-[80%] lg:w-[50%]'>
 <div className='relative flex '>
+
 <img src='../images/booking/Calendar.svg' className='absolute  top-3 right-3 pointer-events-none select-none'/>
 <input
  className='outline-none bg-inputbg w-full py-[10px] rounded-[8px] pl-3 cursor-pointer border-bookingborder border-[1px] border-solid' 
  readOnly
  placeholder='Care Start Date'
- value={date === null ? '' : `${format(date, "dd/MM/yyyy")}` }
+ value={ dateDisplay}
  onClick={calendar}
  id='extractCalendarTime'
  />
 </div>
 </div>
 
-
+<section onClick={calendar} ref={closeCalendarRef}>
 {togglecalendar &&
-<section onClick={calendar}>
 <Calendar 
 showMonthAndYearPickers={false} 
 onChange={item => setDate(item)}
@@ -274,8 +282,8 @@ date={date}
 className='shadow-xl absolute z-[1]'
 minDate={ new Date(new Date().setDate(new Date().getDate() + 1))}
 />
-</section>
 }
+</section>
 
 </section>
 </div>
@@ -289,16 +297,12 @@ minDate={ new Date(new Date().setDate(new Date().getDate() + 1))}
 </form>
 
 
-</main>
-</main>
 
-{/* ----------------------------------------------------------------------------------------------------------------- */}
+    </main>
+    <Footer {...footerDetails}/>
 
-
- <Footer {...footerDetails}/>
-
-    </>
-  )
+  </div>
+    )
 }
 
 export default page
